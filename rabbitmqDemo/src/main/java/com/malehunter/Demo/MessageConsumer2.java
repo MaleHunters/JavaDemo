1a7d2b6170204878aaf261e07fde48c0000e6b01
MaleHunter <peitong_cong@163.com>
2021-01-30 18:54:59 +0800

    分裂模式 生产者与消费者

---
+++ rabbitmqDemo/src/main/java/com/malehunter/Demo/MessageConsumer2.java
@@ -0,0 +1,23 @@
+package com.malehunter.Demo;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+/**
+ * @Auther: MaleHunter
+ * @Date: 2021/1/30 18:22
+ * @Package: com.malehunter.Demo
+ * @CurrentProject: rabbitmqDemo
+ * @version: 1.0
+ */
+// 消息监听类
+public class MessageConsumer2 implements MessageListener {
+  @Override
+  public void onMessage(Message message) {
+    System.out.println("MessageConsumer2");
+    byte[] body = message.getBody();
+    String s = new String(body);
+    System.out.println(s);
+
+  }
+}
