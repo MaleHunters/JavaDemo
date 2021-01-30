acc0513e29ab2fdb3ffd4a4a9fdc31c19cdd67b3
MaleHunter <peitong_cong@163.com>
2021-01-30 18:35:08 +0800

    2.消息生产者配置

--- rabbitmqDemo/src/main/java/com/malehunter/Demo/Test1.java
+++ rabbitmqDemo/src/main/java/com/malehunter/Demo/Test1.java
@@ -15,9 +15,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 // 消息生产者
 public class Test1 {
   public static void main(String[] args) {
-    ApplicationContext context =
-        new ClassPathXmlApplicationContext(
-            "applicationContext-rabbitmq-producer.xml");
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-rabbitmq-producer.xml");
     RabbitTemplate rabbitTemplate =(RabbitTemplate)context.getBean("rabbitTemplate");
     rabbitTemplate.convertAndSend("","queue.test","直接模式测试");
     ((ClassPathXmlApplicationContext) context).close();
